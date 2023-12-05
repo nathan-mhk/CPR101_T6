67eb2e834dc08a0e644123334ca81cc10276ec2a
Nathan <mhk.nathan@gmail.com>
2023-12-05 15:26:43 -0500

    Added Manipulating v2

--- src/manipulating.c
+++ src/manipulating.c
@@ -19,6 +19,54 @@ void manipulating(void) {
 			strcat(string1, string2);
 			printf("Concatenated string is \'%s\'\n", string1);
 		}
+
+
+
+
 	} while (strcmp(string1, "q") != 0);
 	printf("*** End of Concatenating Strings Demo ***\n\n");
+
+
+	// V2
+	printf("*** Start of Comparing Strings Demo ***\n");
+	char compare1[BUFFER_SIZE];
+	char compare2[BUFFER_SIZE];
+	int result;
+
+	do {
+		printf("Type the 1st string to compare (q - to quit):\n");
+		fgets(compare1, BUFFER_SIZE, stdin);
+		compare1[strlen(compare1) - 1] = '\0';
+		if (strcmp(compare1, "q") != 0) {
+			printf("Type the 2nd string to compare:\n");
+			fgets(compare2, BUFFER_SIZE, stdin);
+			compare2[strlen(compare2) - 1] = '\0';
+			result = strcmp(compare1, compare2);
+			if (result < 0)
+				printf("\'%s\' string is less than \'%s\'\n", compare1, compare2);
+			else if (result == 0)
+				printf("\'%s\' string is equal to \'%s\'\n", compare1, compare2);
+			else
+				printf("\'%s\' string is greater than \'%s\'\n", compare1, compare2);
+		}
+
+	} while (strcmp(compare1, "q") != 0);
+	printf("*** End of Comparing Strings Demo ***\n\n");
+
+
+
 }
+
+
+
+
+
+	
+
+
+
+
+
+
+
+
