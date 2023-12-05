68254695dafe3cb93cebbf4e7871eea9111db08a
Nathan <mhk.nathan@gmail.com>
2023-12-05 15:23:50 -0500

    Uncommented V1

--- src/tokenizing.c
+++ src/tokenizing.c
@@ -5,25 +5,26 @@
 
 // V1
 void tokenizing(void) {
-    // printf("*** Start of Tokenizing Words Demo ***\n");
-    // char    words[BUFFER_SIZE];
-    // char*   nextWord = NULL;
-    // int     wordsCounter;
-    // do {
-    //     printf("Type a few words separated by space (q - to quit):\n");
-    //     fgets(words, BUFFER_SIZE, stdin);
-    //     words[strlen(words) - 1] = '\0';
-    //     if (strcmp(words, "q") != 0) {
-    //         nextWord = strtok(words, " ");
-    //         wordsCounter = 1;
-    //         while (nextWord) {
-    //             printf("Word #%d is \'%s\'\n", wordsCounter++, nextWord);
-    //             nextWord = strtok(NULL, " ");
-    //         }
-    //     }
-    // } while (strcmp(words, "q") != 0);
-    // printf("*** End of Tokenizing Words Demo ***\n\n");
+    printf("*** Start of Tokenizing Words Demo ***\n");
+    char    words[BUFFER_SIZE];
+    char*   nextWord = NULL;
+    int     wordsCounter;
+    do {
+        printf("Type a few words separated by space (q - to quit):\n");
+        fgets(words, BUFFER_SIZE, stdin);
+        words[strlen(words) - 1] = '\0';
+        if (strcmp(words, "q") != 0) {
+            nextWord = strtok(words, " ");
+            wordsCounter = 1;
+            while (nextWord) {
+                printf("Word #%d is \'%s\'\n", wordsCounter++, nextWord);
+                nextWord = strtok(NULL, " ");
+            }
+        }
+    } while (strcmp(words, "q") != 0);
+    printf("*** End of Tokenizing Words Demo ***\n\n");
 
+// V2
     printf("*** Start of Tokenizing Phrases Demo ***\n");
     char    phrases[BUFFER_SIZE];
     char*   nextPhrase = NULL;
