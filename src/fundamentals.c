b4acf0084c34760900c53ae26a74653bb18340f3
Nathan <mhk.nathan@gmail.com>
2023-12-09 13:53:51 -0500

    Fundamentals v2

--- src/fundamentals.c
+++ src/fundamentals.c
@@ -29,4 +29,16 @@ void fundamentals(void) {
 		}
 	} while (strcmp(buffer1, "q") != 0);
 	printf("*** End of Indexing Strings demo ***\n\n");
+
+	// V2
+	printf("*** Start of Measuring Strings Demo ***\n");
+	char buffer2[BUFFER_SIZE];
+	do {
+		printf("Type a string (q - to quit):\n");
+		fgets(buffer2, BUFFER_SIZE, stdin);
+		buffer2[strlen(buffer2) - 1] = '\0';
+		if (strcmp(buffer2, "q") != 0)
+			printf("The length of \'%s\' is %d characters\n", buffer2, (int)strlen(buffer2));
+	} while (strcmp(buffer2, "q") != 0);
+	printf("*** End of Measuring Strings Demo ***\n\n");
 }
